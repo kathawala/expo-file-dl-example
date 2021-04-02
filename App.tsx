1050f203050c5f8090bef9d4bfbc0da8757cdd2f
Farhan Kathawala <farhank@stanford.edu>
2021-04-02 17:59:38 -0500

    Updated deps, added Notification permission puller for iOS

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import { AndroidImportance, AndroidNotificationVisibility, NotificationChannel, NotificationChannelInput, NotificationContentInput } from 'expo-notifications';
-import { downloadToFolder } from 'expo-file-dl';
+import { StatusBar } from "expo-status-bar";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import {
+  AndroidImportance,
+  AndroidNotificationVisibility,
+  NotificationChannel,
+  NotificationChannelInput,
+  NotificationContentInput,
+} from "expo-notifications";
+import { downloadToFolder } from "expo-file-dl";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -14,14 +20,16 @@ Notifications.setNotificationHandler({
   }),
 });
 
-const channelId = "DownloadInfo"
+const channelId = "DownloadInfo";
 
 export default function App() {
   const [uri, setUri] = useState("");
   const [filename, setFilename] = useState("");
 
   async function setNotificationChannel() {
-    const loadingChannel: NotificationChannel | null = await Notifications.getNotificationChannelAsync(channelId);
+    const loadingChannel: NotificationChannel | null = await Notifications.getNotificationChannelAsync(
+      channelId
+    );
 
     // if we didn't find a notification channel set how we like it, then we create one
     if (loadingChannel == null) {
@@ -29,11 +37,14 @@ export default function App() {
         name: channelId,
         importance: AndroidImportance.HIGH,
         lockscreenVisibility: AndroidNotificationVisibility.PUBLIC,
-        sound: 'default',
+        sound: "default",
         vibrationPattern: [250],
-        enableVibrate: true
+        enableVibrate: true,
       };
-      await Notifications.setNotificationChannelAsync(channelId, channelOptions);
+      await Notifications.setNotificationChannelAsync(
+        channelId,
+        channelOptions
+      );
     }
   }
 
@@ -41,13 +52,25 @@ export default function App() {
     setNotificationChannel();
   });
 
-  // IMPORTANT: You MUST attain CAMERA_ROLL permissions for the file download to succeed
+  // IMPORTANT: You MUST obtain MEDIA_LIBRARY permissions for the file download to succeed
   // If you don't the downloads will fail
-  async function getCameraRollPermissions() {
-    await Permissions.askAsync(Permissions.CAMERA_ROLL);
+  async function getMediaLibraryPermissions() {
+    await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
   }
+
+  // You also MUST obtain NOTIFICATIONS permissions to show any notification
+  // to the user. Please read the docs for more on permissions for notifications
+  // https://docs.expo.io/versions/latest/sdk/notifications/#fetching-information-about-notifications-related-permissions
+  async function getNotificationPermissions() {
+    await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  }
+
   useEffect(() => {
-    getCameraRollPermissions();
+    getMediaLibraryPermissions();
+  });
+
+  useEffect(() => {
+    getNotificationPermissions();
   });
 
   return (
@@ -57,37 +80,39 @@ export default function App() {
         value={uri}
         placeholder="http://www.example.com/image.jpg"
         onChangeText={(uri) => setUri(uri)}
-        style={{width: '80%'}}
+        style={{ width: "80%" }}
       />
       <TextInput
         value={filename}
         placeholder="image.jpg"
         onChangeText={(filename) => setFilename(filename)}
-        style={{width: '80%'}}
+        style={{ width: "80%" }}
       />
-      <Button title='Download' onPress={async () => {
-        // You can also call downloadToFolder with custom notification content, or without any notifications sent at all
+      <Button
+        title="Download"
+        onPress={async () => {
+          // You can also call downloadToFolder with custom notification content, or without any notifications sent at all
 
-        // ***************************
-        // custom notification content
-        // ***************************
-        // const customNotifInput: {downloading: NotificationContentInput, finished: NotificationContentInput, error: NotificationContentInput} = {
-        //   downloading: { title: "Custom title 1", body: 'Custom body 1', color: '#06004a' },
-        //   finished: { title: "Custom title 2", body: 'Custom body 2', color: '#004a00' },
-        //   error: { title: "Custom title 3", body: 'Custom body 3', color: '#810002' }
-        // };
-        // await downloadToFolder(uri, filename, "Download", channelId, { notification: "custom" }, customNotifInput);
+          // ***************************
+          // custom notification content
+          // ***************************
+          // const customNotifInput: {downloading: NotificationContentInput, finished: NotificationContentInput, error: NotificationContentInput} = {
+          //   downloading: { title: "Custom title 1", body: 'Custom body 1', color: '#06004a' },
+          //   finished: { title: "Custom title 2", body: 'Custom body 2', color: '#004a00' },
+          //   error: { title: "Custom title 3", body: 'Custom body 3', color: '#810002' }
+          // };
+          // await downloadToFolder(uri, filename, "Download", channelId, { notification: "custom" }, customNotifInput);
 
-        // ****************
-        // no notifications
-        // ****************        
-        // await downloadToFolder(uri, filename, "Download", channelId, { notification: "none" });
+          // ****************
+          // no notifications
+          // ****************
+          // await downloadToFolder(uri, filename, "Download", channelId, { notification: "none" });
 
-        // *******
-        // default
-        // *******
-        await downloadToFolder(uri, filename, "Download", channelId);
-      }}
+          // *******
+          // default
+          // *******
+          await downloadToFolder(uri, filename, "Download", channelId);
+        }}
       />
     </View>
   );
@@ -96,8 +121,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
